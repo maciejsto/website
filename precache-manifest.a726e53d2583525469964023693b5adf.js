c4e05eb4c8f2e21b5d7fbe4b33108766bdf030cf
maciejsto <maciejsto@gmail.com>
2019-11-10 23:07:37 +0100

    Updates

---
+++ precache-manifest.a726e53d2583525469964023693b5adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc78e3754699214683d74c05daec0377",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "6637ac2e722ed57b2bce",
+    "url": "/website/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d1dac67a4280af3edc72",
+    "url": "/website/static/js/2.2d72ee5c.chunk.js"
+  },
+  {
+    "revision": "6637ac2e722ed57b2bce",
+    "url": "/website/static/js/main.4679e0ce.chunk.js"
+  },
+  {
+    "revision": "596830d9d2746410f827",
+    "url": "/website/static/js/runtime-main.16cc0d70.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/website/static/media/logo.25bf045c.svg"
+  }
+]);
