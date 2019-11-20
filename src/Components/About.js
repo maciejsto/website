edea379cebb50bf3b4c9bb3cbb3a927b7fab149a
maciejsto <maciejsto@gmail.com>
2019-11-20 21:48:56 +0100

    centered images in about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,15 +8,15 @@ class About extends Component {
           <div className="container">
           <section className="about testimonials text-center bg-light">
               <h2 className="mb-5">O Nas</h2>
-                <div className="row">
-                  <div className="col-lg-3">
+                <div className="row align-items-center">
+                  <div className="col col-md-4 col-lg-3 offset-md-2">
                     <div className="testimonial-item mx-auto mb-5 mb-lg-0">
                       <img className="img-fluid rounded-circle mb-5" src={Img1} alt=""/>
                       <h5>Psycholog1</h5>
                       <p className="font-weight-light mb-0">"krótki opis"</p>
                     </div>
                   </div>
-                  <div className="col-lg-3">
+                  <div className="col col-md-4 col-lg-3 offset-md-2">
                     <div className="testimonial-item mx-auto mb-5 mb-lg-0">
                       <img className="img-fluid rounded-circle mb-3" src={Img2} alt=""/>
                       <h5>Psycholog2</h5>
